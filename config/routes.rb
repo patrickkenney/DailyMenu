bff18f8dd549e64fc6d0e36c9cd24ad2fe39307f
Elliot Murphy <elliot.murphy@gmail.com>
2013-06-16 19:44:34 -0400

    Disable broken routes so index works again.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 MenuMaster::Application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
+  #devise_for :admin_users, ActiveAdmin::Devise.config
+  #ActiveAdmin.routes(self)
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
